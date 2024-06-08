d76f21490e2915ac22161a6f7082bef2f85c1524
darrenjames8793 <48378312+darrenjames8793@users.noreply.github.com>
2024-06-08 23:08:09 +0530

    Update CreatePost.jsx

--- client/src/pages/CreatePost.jsx
+++ client/src/pages/CreatePost.jsx
@@ -26,7 +26,7 @@ const CreatePost = () => {
      if(form.prompt){
       try {
         setGeneratingImg(true);
-        const response = await fetch('http://localhost:8080/api/v1/dalle',{
+        const response = await fetch('https://image-gen-bay-theta.vercel.app/api/v1/dalle',{
           method: 'POST',
           headers: {
             'Content-Type' : 'application/json',
@@ -57,7 +57,7 @@ const CreatePost = () => {
        
        try {
         
-       const response = await fetch('http://localhost:8080/api/v1/post',{
+       const response = await fetch('https://image-gen-bay-theta.vercel.app/api/v1/post',{
           method: 'POST',
           headers: {
             'Content-Type' : 'application/json',
