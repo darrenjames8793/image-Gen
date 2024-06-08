82bb240b7fa447dfc9b8525b35ff17eb5d1aac7b
darrenjames8793 <48378312+darrenjames8793@users.noreply.github.com>
2024-06-08 23:08:51 +0530

    Update Home.jsx

--- client/src/pages/Home.jsx
+++ client/src/pages/Home.jsx
@@ -32,7 +32,7 @@ const Home = () => {
        setLoading(true);
 
        try {
-        const response = await fetch('http://localhost:8080/api/v1/post',{
+        const response = await fetch('https://image-gen-bay-theta.vercel.app/api/v1/post',{
 
           method: 'GET',
           headers: {
